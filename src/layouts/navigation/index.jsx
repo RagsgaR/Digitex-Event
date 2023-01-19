c8fedbf41a319402e9b5467b47d5719c017e67e2
RAGSGAR <ragsgar@localhost.localdomain>
2023-01-19 17:15:38 +0300

    modified:   src/App.css 	modified:   src/App.js 	modified:   src/assets/Creation/CaskCercle.png 	modified:   src/assets/Creation/Ecole.png 	modified:   src/assets/Creation/Parameter2.png 	modified:   src/assets/Creation/ParameterCercle.png 	modified:   src/assets/Creation/internet2.png 	modified:   src/assets/Creation/statistiqueMoney.png 	modified:   src/layouts/footer/index.jsx 	modified:   src/layouts/navigation/index.jsx 	modified:   src/pages/Programme.jsx 	modified:   "src/pages/Th\303\251matique.jsx" 	modified:   src/pages/index.jsx 	modified:   src/sections/home/Engagement.section.jsx 	modified:   src/sections/home/Key.section.jsx 	modified:   src/sections/home/Presentation.section.jsx 	modified:   src/sections/home/hero.jsx 	modified:   src/sections/home/organisateur.section.jsx 	src/assets/Creation/internet.png 	src/assets/Creation/location.png

--- src/layouts/navigation/index.jsx
+++ src/layouts/navigation/index.jsx
@@ -1,11 +1,11 @@
-import React from "react"
-import {useMediaQuery, useTheme,Box } from "@mui/material";
+import React from "react";
+import { useMediaQuery, useTheme, Box } from "@mui/material";
 import { motion, useViewportScroll } from "framer-motion";
 import { useState } from "react";
 import NavDrawer from "./drawer";
-import {useDimensions} from "./drawer/use-dimensions"
-import { useNavigate } from 'react-router-dom';
-import Logo from '../../assets/logo_digitex.png';
+import { useDimensions } from "./drawer/use-dimensions";
+import { useNavigate } from "react-router-dom";
+import Logo from "../../assets/logo_digitex.png";
 import "./Nav.css";
 
 const navStyles = {
@@ -19,16 +19,14 @@ const navStyles = {
   zIndex: 9,
 };
 
-
 export function Navigation() {
   const theme = useTheme();
   const { scrollY } = useViewportScroll();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
   const containerRef = React.useRef(null);
   const { height } = useDimensions(containerRef);
-  const [option,setOption]=React.useState("/")
-  const navigate = useNavigate()
-
+  const navigate = useNavigate();
+  const [path, setPath] = React.useState(window.location.pathname);
   const [hidden, setHidden] = useState(false);
 
   function update() {
@@ -50,31 +48,65 @@ export function Navigation() {
     return scrollY.onChange(() => update());
   });
 
-
-
-
-
-
+  React.useEffect(() => {
+    setPath(window.location.pathname);
+  }, [window.location.pathname]);
 
   return (
     <motion.div
-    className="nav"
-    custom={height}
-    ref={containerRef}
+      className="nav"
+      custom={height}
+      ref={containerRef}
       variants={variants}
       animate={hidden ? "hidden" : "visible"}
       transition={{ ease: [0.1, 0.25, 0.3, 1], duration: 0.6 }}
-      style={{ ...navStyles, height: isMobile ? "4rem" : "5rem",width:"100vw",zIndex:"2000",background:"rgba(145, 235, 255, 0.65)",boxShadow:"3px 3px 3px #0000007a" }}
-    >
-      <div onClick={()=>setOption('/')} class="logo"><Box class={option === "/" ? "active":"test"} onClick={()=>navigate('/')} component={"img"} sx={{width:{xs:"250",sm:"125px"},height:"auto", alignItems:"center"}} src={Logo} alt=""/></div> 
-       {/*<div class="logo">Digi<Box component={"span"} sx={{color:"#0040ff"}}>tex</Box></div> */}
-      <Box sx={{display:{xs:"block",md:"none"}}}>
-      <NavDrawer/>
+      style={{
+        ...navStyles,
+        height: isMobile ? "4rem" : "5rem",
+        width: "100vw",
+        zIndex: "2000",
+        background: "rgba(145, 235, 255, 0.65)",
+        boxShadow: "3px 3px 3px #0000007a",
+      }}>
+      <div class="logo">
+        <Box
+          class={path === "/" ? "active" : "test"}
+          onClick={() => navigate("/")}
+          component={"img"}
+          sx={{
+            width: { xs: "250", sm: "125px" },
+            height: "auto",
+            alignItems: "center",
+          }}
+          src={Logo}
+          alt=""
+        />
+      </div>
+      <Box sx={{ display: { xs: "block", md: "none" } }}>
+        <NavDrawer />
       </Box>
       <ul>
-        <li onClick={()=>setOption('/')}><button class={option === "/" ? "active":"test"} onClick={()=>navigate('/')}>Accueil</button></li>
-        <li onClick={()=>setOption('/Thématique')}><button class={option === "/Thématique" ? "active" : "test"} onClick={()=>navigate("/Thematics")}>Thématique</button></li>
-        <li onClick={()=>setOption('/Programme')}><button class={option === "/Programme" ? "active" : "test"} onClick={()=>navigate("/Programme")}>Programme</button></li>
+        <li>
+          <button
+            class={path === "/" ? "active" : "test"}
+            onClick={() => navigate("/")}>
+            Accueil
+          </button>
+        </li>
+        <li>
+          <button
+            class={path === `/Thematics` ? "active" : "test"}
+            onClick={() => navigate(`/Thematics`)}>
+            Thématique
+          </button>
+        </li>
+        <li>
+          <button
+            class={path === `/Programme` ? "active" : "test"}
+            onClick={() => navigate(`/Programme`)}>
+            Programme
+          </button>
+        </li>
       </ul>
     </motion.div>
   );
